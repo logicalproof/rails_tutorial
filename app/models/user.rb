3bcdf589cb93ec7d9568aa6faf8c509f1b99ea00
Nick Sandberg <nss118@gmail.com>
2012-03-29 19:31:11 -0700

    Finished user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,6 @@ class User < ActiveRecord::Base
           uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+ 
 end
